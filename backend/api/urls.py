a7dd0c32878ae64a85f19432d730b5c152233313
Dzmitry-Radziuk <mitia.radiuk@yandex.ru>
2025-05-27 22:55:18 +0300

    Foodgram final send fo review #2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
 
 
